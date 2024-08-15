4966f9766996f4409a6ad75876bc7514b32f246f
Ahmed_hamdy <118023886+SiNfuuL@users.noreply.github.com>
2024-08-15 22:08:14 +0300

    chore: Update Kotlin version to 2.0.10

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio dio = Dio();
+
+  Future<Response> post(
+      {required String url,
+      required body,
+      required String token,
+      String? contentType}) async {
+    // Make a POST request
+
+    var response = await dio.post(
+      url,
+      data: body,
+      options: Options(
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        contentType: contentType,
+      ),
+    );
+    return response;
+  }
+}
